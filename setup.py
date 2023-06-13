6a136f44f5ea111876e07867b403edf11cf51fcb
danielpdwalker <danielpdwalker@gmail.com>
2023-06-13 12:07:58 +0100

    Updated setup.py package name, and file paths to bring files in from. Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="analyze-example",
-    version="0.1.0",
+    name="analyze-snowflake-monitoring",
+    version="0.2.0",
     description="Meltano project file bundle of Matatika datasets for monitoring Snowflake",
     packages=find_packages(),
     package_data={
         "bundle": [
             "analyze/datasets/snowflake-monitoring/*.yml",
-            "analyze/channels/*.yml"
+            "analyze/channels/*.yml",
+            "analyze/pipelines/*.yml",
+            "analyze/datastores/*.yml"
         ]
     },
 )
